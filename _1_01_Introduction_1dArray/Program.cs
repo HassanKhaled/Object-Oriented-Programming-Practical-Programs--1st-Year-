ef74e0b64b05183087d572560f9f3489d5c06936
hassan khaled <hasan.khaled.fcis@gmail.com>
2023-02-17 16:27:12 +0200

    testing
    just trying to test the changes detection

--- _1_01_Introduction_1dArray/Program.cs
+++ _1_01_Introduction_1dArray/Program.cs
@@ -14,11 +14,12 @@ namespace _1_1_Array_Introduction
     {
         static void Main(string[] args)
         {
-            // 1.1 Array Introduction
+            //1.1 Array Introduction
             double d1, d2, d3;
             d1 = 10.5;
             d2 = 15.5;
             d3 = 9.5;
+
         }
 
     }
