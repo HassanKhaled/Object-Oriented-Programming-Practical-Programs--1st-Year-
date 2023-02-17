ca31c52d657d837b42b2e4a0b0cf0dc5a63df808
hassan khaled <hasan.khaled.fcis@gmail.com>
2023-02-17 16:10:35 +0200

    added first lab
    this is the first lab content, I hope you will like it

---
+++ _1_14_Count_Odd_Even_Elements_Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_14_Count_Odd_Even_Elements_Array
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1.14 Count odd and even and specifice elements inside array 
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Numbers of Odd Elements is {0}", countOdd(arr));
+            Console.WriteLine("Numbers of Even Elements is {0}", countEven(arr));
+            Console.WriteLine("Numbers of 2 in array is {0}", countElement(arr, 2));
+        }
+
+        static int countElement(int[] arr, int element)
+        {
+
+            int count = 0;
+            foreach (var i in arr)
+                if (i == element)
+                    count++;
+            return count;
+        }
+        static int countEven(int[] arr)
+        {
+            int count = 0;
+            foreach (var i in arr)
+                if (i % 2 == 0)
+                    count++;
+            return count;
+        }
+        static int countOdd(int[] arr)
+        {
+            int count = 0;
+            foreach (var i in arr)
+                if (i % 2 == 1)
+                    count++;
+            return count;
+        }
+    }
+}
---
+++ _1_6_Reading_Array_Elements_From_User/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_6_Reading_Array_Elements_From_User_For_Loop
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 reading elements from user using for loop 
+            int[] nums = new int[3];
+            Console.WriteLine();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine("enter element number" + i);
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int item in nums)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
